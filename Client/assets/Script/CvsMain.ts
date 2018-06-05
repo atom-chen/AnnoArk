15384050a6080debbf70d1f95eb9ef95e3a1fe98
Stephen Zhou <stv1024@gmail.com>
2018-06-05 18:32:32 +0800

    try to fit all screen

--- Client/assets/Script/CvsMain.ts
+++ Client/assets/Script/CvsMain.ts
@@ -24,4 +24,23 @@ export default class CsvMain extends cc.Component {
             }
         })
     }
+
+    // update() {
+    //     let visibleSize = cc.view.getVisibleSize();
+    //     console.log('view, size', cc.view.getVisibleSize(),
+    //         this.node.position,
+    //         this.node.getContentSize());
+    //     if (visibleSize.width >= visibleSize.height) {
+    //         this.node.rotation = 0;
+    //         this.node.scale = 1;
+    //         this.node.setContentSize(1080 / visibleSize.height * visibleSize.width, 1080);
+    //         this.node.position = new cc.Vec2(visibleSize.width/2,visibleSize.height /2);
+    //     } else {
+    //         this.node.rotation = 90;
+    //         this.node.setContentSize(1080 / visibleSize.width * visibleSize.height, 1080);
+    //         this.node.position = new cc.Vec2(visibleSize.width/2,visibleSize.height/2);
+    //     console.log('this.node.position',this.node.position);
+    //     this.node.scale = visibleSize.width / 1080;
+    //     }
+    // }
 }
